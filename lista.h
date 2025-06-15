51420b6408f30a8f4145088177091a0ec53b19fd
Mateus Biancardi <mateusbbdasilva@gmail.com>
2025-06-15 18:28:34 -0300

    feat: lista genérica duplamente encadeada

---
+++ lista.h
@@ -0,0 +1,14 @@
+/*
+ *
+ * Lista Duplamente Encadeada para itens genéricos
+ * Autores: Mateus Biancardi e Rafaela Capovilla
+ *
+ */
+
+#ifndef LISTA_H
+#define LISTA_H
+
+typedef struct celula Celula;
+typedef struct lista Lista;
+
+#endif // LISTA_H
